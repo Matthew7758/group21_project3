a0cbc088bef27d0cbe2f7ab2f86a562d63366602
God <entei567@gmail.com>
2021-09-01 21:07:31 -0400

    Fixed font types on View and added method stubs to remove errors. TODO: Write MainActivity.kt and add a horizontal layout.

--- app/src/main/java/com/group21/android/basketballcounter/MainActivity.kt
+++ app/src/main/java/com/group21/android/basketballcounter/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.group21.android.basketballcounter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun threeTeam1(view: View) {}
+    fun twoTeam1(view: View) {}
+    fun oneTeam1(view: View) {}
+    fun threeTeam2(view: View) {}
+    fun twoTeam2(view: View) {}
+    fun oneTeam2(view: View) {}
 }
