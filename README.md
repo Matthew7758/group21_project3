1687b2443d9e9c5f8dd32a0c7d6e11302686f53d
theratzoo <lderatzou@wpi.edu>
2021-09-20 19:48:20 -0400

    added readme and databse as per Part5

---
+++ README.md
@@ -0,0 +1,5 @@
+## README
+
+Parts 1-6 are stored as branches.
+
+The database directory from part 5 can be found in this directory. It is named "database".
---
+++ database/GameDao.kt
@@ -0,0 +1,24 @@
+package com.group21.android.basketballcounter.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.group21.android.basketballcounter.Game
+import java.util.*
+
+@Dao
+interface GameDao {
+    @Query("SELECT * FROM game")
+    fun getAll(): LiveData<List<Game>>
+
+    @Query("SELECT * FROM game where id LIKE :id")
+    fun findByID(id : UUID): LiveData<Game>
+
+    @Insert
+    fun insert(game: Game)
+
+    @Delete
+    fun delete(game : Game)
+
+    @Update
+    fun update(game : Game)
+}
